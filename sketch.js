d73652e6378860ef9ac8f587121f4073f409fbaa
mrjloswald-avenues <jason.oswald@avenues-ny.org>
2024-10-24 13:44:34 -0400

    Update sketch.js
    Added color changing, updated keyPressed, and added movement

--- sketch.js
+++ sketch.js
@@ -1,21 +1,46 @@
 let x
 let y
 let radius
+let r 
+let g 
+let b 
+let dx 
+let dy 
 
 function setup() {
   createCanvas(400, 400);
-  x = random(width) // [0,width)
-  y = random(height)
-  radius = random(10,20)
+  createNewCircle()
 }
 
 function draw() {
   background(220);
+  fill(r,g,b)
   circle(x,y,radius)
+  x = x + dx
+  y = y + dy
 }
 
 function keyPressed() {
+  if( key === "n" ) { // some T/F question
+    createNewCircle()
+  }
+  
+  if( key === "c" ) {
+    changeColor()
+  }
+}
+
+function changeColor() {
+  r = random(255)
+  g = random(255)
+  b = random(255)  
+}
+
+function createNewCircle() {
+  changeColor()
+  dx = random(-1,1)
+  dy = random(-1,1)
   x = random(width) // [0,width)
   y = random(height)
-  radius = random(10,20)  
+  radius = random(10,20)        
 }
